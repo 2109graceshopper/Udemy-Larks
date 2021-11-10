f83401108cc68ad2b1a12bd0f8a3371c4c3dda25
Jian <jiankuang7@gmail.com>
2021-11-10 14:54:32 -0500

    Updating comments

--- client/App.js
+++ client/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import Navbar from './components/Navbar'
 import Routes from './Routes'
-import Registration from './components/Registration/Registration'
+// import Registration from './components/Registration/Registration'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
-      <Registration /> //Testing purposes -- remove later
+      {/* <Registration /> //Testing purposes -- remove later */}
     </div>
   )
 }
