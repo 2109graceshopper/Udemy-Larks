1039311e7d9b6acf585d4bb60d670f9b8ab0af02
Jian <jiankuang7@gmail.com>
2021-11-10 13:18:20 -0500

    adding registration features basic

--- client/App.js
+++ client/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Navbar from './components/Navbar'
 import Routes from './Routes'
+import Registration from './components/Registration/Registration'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <Registration />
     </div>
   )
 }
