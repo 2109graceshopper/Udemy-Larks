170ef693a8928cef1d0d9a096f361b547daf6dd8
totsuka1 <takumi.otsuka@gmail.com>
2021-11-10 18:40:49 -0500

    Created AllProducts Component

---
+++ client/components/AllProducts.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+// import {fetchProducts} from "....."
+
+export class AllProducts extends React.Component {}
