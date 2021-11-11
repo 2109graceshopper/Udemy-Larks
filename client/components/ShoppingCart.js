bfbc76076cdf7d6d0b4e1d7bcb9e56f574a5230e
totsuka1 <takumi.otsuka@gmail.com>
2021-11-11 12:05:09 -0500

    Shopping Cart Skeleton

---
+++ client/components/ShoppingCart.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+export class ShoppingCart extends React.Component {}
+
+// const mapStateToProps = (state) => {
+//   return {
+//     products: state.userid.products
+// (will likely be something different)
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     removeFromCart: (productId) =>
+//     checkOut: () =>
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart)
