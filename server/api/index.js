e6d09dcf4b607421630fd066e696d464d45d7f8e
Jian <jiankuang7@gmail.com>
2021-11-11 17:47:25 -0500

    Updating seed function

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router()
-module.exports = router
 
+router.use('/orders', require('./orders'))
 router.use('/users', require('./users'))
+// router.use('/videos', require('./videos'))
+
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
   next(error)
 })
+
+module.exports = router
