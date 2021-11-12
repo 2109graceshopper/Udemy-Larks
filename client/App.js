ee906f2f1458681fdb1dfa31da67c9406a8e108b
totsuka1 <takumi.otsuka@gmail.com>
2021-11-12 13:57:29 -0500

    Added Footer placeholder

--- client/App.js
+++ client/App.js
@@ -3,12 +3,14 @@ import React from "react";
 
 import Header from "./components/Header";
 import Routes from "./Routes";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Routes />
+      <Footer />
     </div>
   );
 };
---
+++ client/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Router, Route, Link, Switch } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <nav>
+      <div>Footer Placeholder</div>
+    </nav>
+  );
+};
+
+export default Footer;
