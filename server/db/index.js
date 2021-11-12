603611d7aa11a90d100c6c2cf7b079e5e3e6f80f
Jian <jiankuang7@gmail.com>
2021-11-12 00:09:42 -0500

    Updating routes

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('../db')
 const User = require('./models/User')
 const Video = require('./models/Video')
 const Order = require('./models/Order')
