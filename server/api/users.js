960924d257e8421181cbf7e14f6b1ea7624ed7e7
Jian <jiankuang7@gmail.com>
2021-11-13 15:19:33 -0500

    Starting Point for updating models

--- server/api/users.js
+++ server/api/users.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
-const { User } = require("../db/index");
+const { User, Order } = require("../db/index");
 
 router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll();
     res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      include: Order,
+    });
+    res.json(user);
   } catch (err) {
     next(err);
   }
