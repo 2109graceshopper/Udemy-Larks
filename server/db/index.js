ecc814f74cb43384776824320939444e9bd442b1
Jian <jiankuang7@gmail.com>
2021-11-13 17:11:29 -0500

    Instantiating new models

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,11 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
-const User = require('./models/User')
-const Video = require('./models/Video')
-const Order = require('./models/Order')
+const db = require("./db");
+const User = require("./models/User");
+const Video = require("./models/Video");
+// const Order = require("./models/Order");
+const { Sequelize } = require("sequelize");
+const { findAllVideos } = require("./models/Video");
 
 //associations could go here!
 //Videos can be part of multiple orders
@@ -15,22 +17,52 @@ const Order = require('./models/Order')
 // OrderUser.belongsTo(User);
 // User.hasMany(OrderUser)
 
+//User Owned Videos
+const UserOwnedVideos = db.define("userownedvideos", {}, { timestamps: false });
 Video.belongsToMany(User, {
   through: {
-    model: Order,
-    unique: false,
+    model: UserOwnedVideos,
   },
 });
-User.belongsToMany(Video, {
-  through: {
-    model: Order,
-    unique: false,
+
+//User has Orders
+const Order = db.define(
+  "order",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    isCart: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
   },
+  { timestamps: false }
+);
+
+User.hasOne(Order);
+
+//Orders have multiple videos
+const OrderVideo = db.define(
+  "ordervideo",
+  {
+    qty: {
+      type: Sequelize.INTEGER,
+      defaultValue: 1,
+    },
+  },
+  { timestamps: false }
+);
+
+Video.belongsToMany(Order, {
+  through: { model: OrderVideo },
 });
 
 module.exports = {
   db,
   User,
   Video,
-  Order
-}
+  Order,
+};
