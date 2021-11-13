3dc5392d3e6de77f906faec163f774f63db7c36e
Tandeep Gill <gill.tandeep@gmail.com>
2021-11-13 12:28:10 +0000

    UserProfile, express route created, awaiting DB design update for DB query

--- server/api/users.js
+++ server/api/users.js
@@ -1,10 +1,27 @@
-const router = require("express").Router();
-const { User } = require("../db/index");
+const router = require('express').Router();
+const { User, Video } = require('../db/index');
 
-router.get("/", async (req, res, next) => {
+// GET /api/users
+router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll();
     res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/users/userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    // DB query to updated once DB design is updated
+    const user = await User.findAll({
+      where: {
+        id: req.params.userId,
+      },
+      include: Video,
+    });
+    res.json(user);
   } catch (err) {
     next(err);
   }
