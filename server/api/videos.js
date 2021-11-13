bb8202786b2ea5a33bbbc2319d13f4f3c718ae1e
Tandeep Gill <gill.tandeep@gmail.com>
2021-11-13 12:45:55 +0000

    SingleVideo, express route added

--- server/api/videos.js
+++ server/api/videos.js
@@ -1,10 +1,24 @@
-const router = require("express").Router();
-const { Video } = require("../db/index");
+const router = require('express').Router();
+const { Video } = require('../db/index');
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const videos = await Video.findAll();
     res.json(videos);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/videos/videoId
+router.get('/:videoId', async (req, res, next) => {
+  try {
+    const video = await Video.findOne({
+      where: {
+        id: req.params.videoId,
+      },
+    });
+    res.json(video);
   } catch (err) {
     next(err);
   }
