2fc127db2ebc52f4082b39f04995cf283880b6e8
Tandeep Gill <gill.tandeep@gmail.com>
2021-11-13 13:50:26 +0000

    SingleVideo, minor updates and added to Routes.js

--- client/components/SingleVideo/SingleVideo.js
+++ client/components/SingleVideo/SingleVideo.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
-import { fetchSingleVideo } from '.../store/singleVideo';
+import { fetchSingleVideo } from '../../store/singleVideo';
 
 class SingleVideo extends React.Component {
   constructor() {
