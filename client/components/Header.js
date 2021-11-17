3a1f1a7f2e5b32b2974a4d0f3c926b6e8c1fd7b4
Tandeep Gill <gill.tandeep@gmail.com>
2021-11-17 17:42:07 +0000

    UserProfile3, token utilized to make request to server

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,59 +1,59 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Router, Route, Link, Switch } from "react-router-dom";
-import { logout } from "../store";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Router, Route, Link, Switch } from 'react-router-dom';
+import { logout } from '../store';
 // import { cartIcon, graceShopperLogo } from "../../public/icons"; //path to wherever we save shared icons/art/logos/etc
 // import { profilePicture } from "../../server/"; //path to wherever we save user photo id
 
-const Header = ({ handleClick, handleChange, isLoggedIn }) => {
+const Header = ({ handleClick, handleChange, isLoggedIn, id }) => {
   return (
     <nav>
-      <Link to="/">
-        <img className="graceShopperLogo" src="/icons/grace-shopper-icon.png" />
+      <Link to='/'>
+        <img className='graceShopperLogo' src='/icons/grace-shopper-icon.png' />
       </Link>
       {/* Categories Drop Down */}
-      <div className="categories-dropdown">
+      <div className='categories-dropdown'>
         <select onChange={handleChange}>
-          <option value="all">All Videos</option>
-          <option value="category1">Category 1</option>
-          <option value="category2">Category 2</option>
-          <option value="category3">Category 3</option>
-          <option value="category4">Category 4</option>
-          <option value="category5">Category 5</option>
+          <option value='all'>All Videos</option>
+          <option value='category1'>Category 1</option>
+          <option value='category2'>Category 2</option>
+          <option value='category3'>Category 3</option>
+          <option value='category4'>Category 4</option>
+          <option value='category5'>Category 5</option>
         </select>
       </div>
-      <Link to="/videos">Videos</Link>
+      <Link to='/videos'>Videos</Link>
       {/* Search Bar */}
       <input
-        className="searchBar"
-        type="text"
-        defaultValue="Search for Videos"
+        className='searchBar'
+        type='text'
+        defaultValue='Search for Videos'
       />
       {/* Favorites? */}
-      <Link to="/cart">
-        <img className="cart-icon" src="/icons/shopping-cart-icon.png" />
+      <Link to='/cart'>
+        <img className='cart-icon' src='/icons/shopping-cart-icon.png' />
       </Link>
 
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/profile">
+          <Link to={`users/${id}`}>
             <img
-              className="profile-picture"
-              src="/icons/profile-picture-placeholder.png"
+              className='profile-picture'
+              src='/icons/profile-picture-placeholder.png'
             />
           </Link>
-          <button className="logInOut" type="button" onClick={handleClick}>
+          <button className='logInOut' type='button' onClick={handleClick}>
             Sign Out
           </button>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/login" className="logInOut">
+          <Link to='/login' className='logInOut'>
             Sign In
           </Link>
-          <Link to="/signup" className="signUp">
+          <Link to='/signup' className='signUp'>
             Sign Up
           </Link>
         </div>
@@ -66,6 +66,7 @@ const Header = ({ handleClick, handleChange, isLoggedIn }) => {
 const mapState = (state) => {
   return {
     isLoggedIn: !!state.auth.id,
+    id: state.auth.id,
   };
 };
 
