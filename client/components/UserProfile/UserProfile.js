d47bd1506d18cc64be6139a7084c3ce4ed9d2db8
Jian <jiankuang7@gmail.com>
2021-11-18 00:23:03 -0500

    Adding what I have so far

--- client/components/UserProfile/UserProfile.js
+++ client/components/UserProfile/UserProfile.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import { fetchUserInfo } from '../../store/user';
-import { me } from '../../store/auth';
+import { fetchUserInfo } from "../../store/user";
+import { me } from "../../store/auth";
 
 class UserProfile extends React.Component {
   constructor() {
@@ -12,7 +12,7 @@ class UserProfile extends React.Component {
 
   componentDidMount() {
     try {
-      const token = window.localStorage.getItem('token');
+      const token = window.localStorage.getItem("token");
       this.props.getUser(this.props.match.params.id, {
         headers: { authorization: token },
       });
@@ -24,29 +24,31 @@ class UserProfile extends React.Component {
   render() {
     const user = this.props.user;
     const userVideos = user.userUniqueVideos ? user.userUniqueVideos : [];
-    console.log(user)
+    console.log(user);
     return !Array.isArray(user) ? (
       <div>
+        <section className="userProfile">
+          <div className="userProfile__card">
+            <h2>User Profile</h2>
+            <img className="" src={user.userimageURL} />
+            <h3>Name: {user.firstName + " " + user.lastName}</h3>
+            <h3>Email: {user.username}</h3>
+          </div>
+        </section>
         <section>
-          <h2>User Profile</h2>
-         <img className='profile-picture' src={user.userimageURL} />
-         <h3>Name: {user.firstName + ' ' + user.lastName}</h3>
-         <h3>Email: {user.username}</h3>
-       </section>
-       <section>
-         <h2>Your Products</h2>
-         {userVideos.map((video) => {
-           return (
-             <div key={video.id}>
-               <Link to={`/videos/${video.id}`}>
-                 <img src={video.imageURL} />
-                 <div>
-                   <h3>{video.title}</h3>
-                   <h4>{video.description}</h4>
-                 </div>
-               </Link>
-             </div>
-           );
+          <h2>Your Products</h2>
+          {userVideos.map((video) => {
+            return (
+              <div key={video.id}>
+                <Link to={`/videos/${video.id}`}>
+                  <img className="userProfile__image" src={video.imageURL} />
+                  <div>
+                    <h3>{video.title}</h3>
+                    <h4>{video.description}</h4>
+                  </div>
+                </Link>
+              </div>
+            );
           })}
         </section>
       </div>
