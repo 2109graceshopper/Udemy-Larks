542ab0cb40f74dbc23c4cf001586517197bcd12c
Jian <jiankuang7@gmail.com>
2021-11-18 10:45:10 -0500

    Header.js

--- client/components/Header.js
+++ client/components/Header.js
@@ -54,7 +54,6 @@ export class Header extends React.Component {
         (video) => video.id
       );
       this.setState({ userOwnedVideos: userOwnedVideoIds });
-      console.log("uservideos", userOwnedVideoIds);
     }
     if (this.props.id !== prevProps.id) {
       await this.props.getUserCart(this.props.id);
